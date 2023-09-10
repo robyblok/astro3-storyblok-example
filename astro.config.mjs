6bd1aba217862e712106b17a762f9e301f34cb52
Roberto Butti <roberto.butti@gmail.com>
2023-09-10 18:28:10 +0200

    First commit with Astro 3, Storyblok and Astro components

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import tailwind from "@astrojs/tailwind";
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
+
+// https://astro.build/config
+export default defineConfig({
+
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      }
+    }),
+    tailwind()
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
